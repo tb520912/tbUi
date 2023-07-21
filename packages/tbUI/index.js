198ce627bd9611c2a20fb5fc6f3462225f4b538b
田斌 <tianbin0912@163.com>
2023-07-21 16:22:47 +0800

    整体打包

---
+++ packages/tbUI/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: 田斌 tianbin0912@163.com
+ * @Date: 2023-07-21 16:15:16
+ * @LastEditors: 田斌 tianbin0912@163.com
+ * @LastEditTime: 2023-07-21 16:16:47
+ * @FilePath: \jQueryProject\packages\tbUI\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import './index.less'
+import tbDialog from '../tbDialog/index.js'
+import tbPopup from '../tbPopup/index.js'
+import tbToast from '../tbToast/index.js'
+export default {
+    tbDialog,
+    tbPopup,
+    tbToast
+}
